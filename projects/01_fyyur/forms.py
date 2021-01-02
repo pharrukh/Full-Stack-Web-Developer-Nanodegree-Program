5b2adeb3b616102537a2868f3ddcc60f44136acf
pharrukh <farrukh.normuradov@gmail.com>
2021-01-02 11:24:12 +0100

    feat: implement all controllers

--- projects/01_fyyur/forms.py
+++ projects/01_fyyur/forms.py
@@ -3,6 +3,82 @@ from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
 from wtforms.validators import DataRequired, AnyOf, URL
 
+STATES = [
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
+
+GENRES=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
+
 class ShowForm(Form):
     artist_id = StringField(
         'artist_id'
@@ -25,59 +101,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices=STATES
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -89,29 +113,8 @@ class VenueForm(Form):
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        choices=GENRES
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
@@ -126,95 +129,24 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices=STATES
     )
     phone = StringField(
-        # TODO implement validation logic for state
         'phone'
     )
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        choices=GENRES
     )
     facebook_link = StringField(
-        # TODO implement enum restriction
         'facebook_link', validators=[URL()]
     )
 
-# TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
+class NewArtistForm(ArtistForm):
+    pass
+
+class NewShowForm(ShowForm):
+    pass
--- projects/01_fyyur/migrations/versions/0df6bdbb0eaa_.py
+++ projects/01_fyyur/migrations/versions/0df6bdbb0eaa_.py
@@ -27,10 +27,116 @@ def upgrade():
     sa.ForeignKeyConstraint(['venue_id'], ['Venue.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    firstVenue = Venue(id=1,
+                       name="The Musical Hop",
+                       city="San Francisco",
+                       state="CA",
+                       genres=["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+                       address="1015 Folsom Street",
+                       phone="123-123-1234",
+                       website="https://www.themusicalhop.com",
+                       facebook_link="https://www.facebook.com/TheMusicalHop",
+                       seeking_talent=True,
+                       seeking_description="We are on the lookout for a local artist to play every two weeks. Please call us.",
+                       image_link="https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+                       )
+
+    secondVenue = Venue(id=3,
+                        name="Park Square Live Music & Coffee",
+                        genres=["Rock n Roll", "Jazz", "Classical", "Folk"],
+                        address="34 Whiskey Moore Ave",
+                        city="San Francisco",
+                        state="CA",
+                        phone="415-000-1234",
+                        website="https://www.parksquarelivemusicandcoffee.com",
+                        facebook_link="https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
+                        seeking_talent=False,
+                        image_link="https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80")
+
+    thirdVenue = Venue(id=2,
+                       name="The Dueling Pianos Bar",
+                       genres=["Classical", "R&B", "Hip-Hop"],
+                       address="335 Delancey Street",
+                       city="New York",
+                       state="NY",
+                       phone="914-003-1132",
+                       website="https://www.theduelingpianos.com",
+                       facebook_link="https://www.facebook.com/theduelingpianos",
+                       seeking_talent=False,
+                       image_link="https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80")
+
+    firstArtist = Artist(id=4,
+                         name="Guns N Petals",
+                         genres=["Rock n Roll"],
+                         city="San Francisco",
+                         state="CA",
+                         phone="326-123-5000",
+                         website="https://www.gunsnpetalsband.com",
+                         facebook_link="https://www.facebook.com/GunsNPetals",
+                         seeking_venue=True,
+                         seeking_description="Looking for shows to perform at in the San Francisco Bay Area!",
+                         image_link="https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80")
+
+    secondArtist = Artist(id=5,
+                          name="Matt Quevedo",
+                          genres=["Jazz"],
+                          city="New York",
+                          state="NY",
+                          phone="300-400-5000",
+                          facebook_link="https://www.facebook.com/mattquevedo923251523",
+                          seeking_venue=False,
+                          image_link="https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80")
+
+    thirdArtist = Artist(id=6,
+                         name="The Wild Sax Band",
+                         genres=["Jazz", "Classical"],
+                         city="San Francisco",
+                         state="CA",
+                         phone="432-325-5432",
+                         seeking_venue=False,
+                         image_link="https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80")
+
+    firstShow = Show(venue_id=1,
+                     artist_id=4,
+                     start_datetime="2019-05-21T21:30:00.000Z")
+
+    secondShow = Show(venue_id=3,
+                      artist_id=5,
+                      start_datetime="2019-06-15T23:00:00.000Z")
+
+    thirdShow = Show(venue_id=3,
+                     artist_id=6,
+                     start_datetime="2035-04-01T20:00:00.000Z")
+
+    fourthShow = Show(venue_id=3,
+                      artist_id=6,
+                      start_datetime="2035-04-08T20:00:00.000Z")
+
+    fifthShow = Show(venue_id=3,
+                     artist_id=6,
+                     start_datetime="2035-04-15T20:00:00.000Z")
+
+    db.session.add(firstVenue)
+    db.session.add(secondVenue)
+    db.session.add(thirdVenue)
+
+    db.session.add(firstArtist)
+    db.session.add(secondArtist)
+    db.session.add(thirdArtist)
+
+    db.session.add(firstShow)
+    db.session.add(secondShow)
+    db.session.add(thirdShow)
+    db.session.add(fourthShow)
+    db.session.add(fifthShow)
+
+    db.session.commit()
     # ### end Alembic commands ###
 
 
 def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
     op.drop_table('Show')
+    
     # ### end Alembic commands ###
